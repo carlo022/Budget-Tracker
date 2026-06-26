557855753618aa581949369b33ba1319c96de843
carlo <carlolalas4@gmail.com>
2026-06-26 22:24:30 +0800

    .env prepared for deployment

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: 'https://budgetingtask.netlify.app/' }));
 app.use(express.json());
 
 // Database Connection
